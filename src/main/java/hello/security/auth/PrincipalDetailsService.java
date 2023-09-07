21dc3278dc41b8efe8bd2720413aa52128ad361b
이훈구 <@DESKTOP-OC9UTOM>
2023-09-07 10:36:44 +0900

    로그인

--- src/main/java/hello/security/auth/PrincipalDetailsService.java
+++ src/main/java/hello/security/auth/PrincipalDetailsService.java
@@ -8,20 +8,27 @@ import org.springframework.stereotype.Service;
 
 import hello.security.model.User;
 import hello.security.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 
 
 /*
  * 시큐리티 설정에서 loginProcessUrl("/login");
  * login 요청이 오면 자동으로 UserDetailsService 타입으로 IoC되어 있는 loadUserByUsername으로  함수가 실행
  */
+@Slf4j
 @Service
 public class PrincipalDetailsService implements UserDetailsService {
 	@Autowired private UserRepository userRepository;
 	
 
+	// 시큐리티 session(내부 Authentication(내부 UserDetails)))
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User userEntity = userRepository.findByUsername(username);  
+		User userEntity = userRepository.findByUsername(username);
+		if(userEntity != null) {
+			log.debug("userEntity={}",userEntity);
+			return new PrincipalDetails(userEntity);
+		}
 		
 		return null;
 	}
